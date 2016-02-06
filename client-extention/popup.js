34507f7e1fd59abaccc7f97c348e1048d421115e
inc-igor <igor.ivanov.taganrog@gmail.com>
2016-02-06 10:15:20 +0300

    Added setting nickname, typing, some changes

--- client-extention/popup.js
+++ client-extention/popup.js
@@ -1,14 +1,119 @@
 var socket = io("https://nog-val-chat.herokuapp.com");
+//var socket = io("http://localhost:3000/");
 
-$('textarea').keypress(function (e) {
+var loginForm = $(".loginForm"),
+	yourName = $("#yourName"),
+	yourEmail = $("#yourEmail"),
+	yourPassword = $("#yourPassword"),
+	myName = $("#myName"),
+	validationError = $("#validation-error"),
+	loginArea = $("#loginArea"),
+	chatArea = $("#chatArea"),
+	messageTextArea = $('textarea'),
+	messagesContainer = $('#messagesContainer');
+
+loginForm.on('submit', function(e){
+				e.preventDefault();
+
+				name = $.trim(yourName.val());
+				email = $.trim(yourEmail.val());
+				pass = $.trim(yourPassword.val());
+				
+				if(name.length < 1){
+					validationError.text("Please enter a nick name longer than 1 character!");
+					return;
+				}
+				if(email.length < 1){
+					validationError.text("Please enter a email longer than 1 character!");
+					return;
+				}
+				if(pass.length < 1){
+					validationError.text("Please enter a password longer than 1 character!");
+					return;
+				}			
+				if(!isValid(email)) {
+					validationError.text("Please enter a valid email!");
+				}
+				else {						
+					loginArea.addClass("hide")
+					chatArea.removeClass("hide");
+					myName.val(name);
+					socket.emit('login', {name: name, email: email, pass: pass});
+				}			
+			});
+			
+messageTextArea.keypress(function (e) {
     if (e.which == 13) {
         e.preventDefault();
-        socket.emit('chat message', $('textarea').val());
-        $('textarea').val('');
+		if(!$.trim(messageTextArea.val())) return;
+        socket.emit('new message', { message: messageTextArea.val(), username: myName.val() });
+        messageTextArea.val('');
     }
 });
-socket.on('chat message', function(msg){
-      $('#ext-message-container').append("<div class='message my'> <div class='time'>" + "Just now" + "</div>" + msg + "</div> <div class='avatar-icon user2'><img src='anton.jpg'></div>");
-      var objDiv = document.getElementById("ext-message-container");
+
+socket.on('new message', function(data){
+	    var who = '';
+	    var avaClass = '';
+
+		if(data.username===myName.val()) {
+			who = 'my';
+			avaClass = 'myAva';
+		}
+		else {
+			who = 'another';
+			avaClass = 'anotherAva';
+		}
+
+      messagesContainer.append("<div class='message " + who + "'>"+ 
+									"<div class='time'>Just now</div>" + data.message + 
+							   "</div>"+ 
+							   "<div class='" + avaClass + " avatar-icon'>"+
+									"<img src='unnamed.jpg'>"+
+							   "</div>");
+      
+	  var objDiv = document.getElementById("messagesContainer");
       objDiv.scrollTop = objDiv.scrollHeight;
-});     
+});  
+
+socket.on('logon message', function(msg){
+      messagesContainer.append("<div class='message logon'>"+ 
+									"<div class='time'>Just now</div>" + msg + 
+							   "</div>");
+      
+	  var objDiv = document.getElementById("messagesContainer");
+      objDiv.scrollTop = objDiv.scrollHeight;
+}); 
+
+socket.on('disconnect message', function(msg){
+      messagesContainer.append("<div class='message disconnect'>"+ 
+									"<div class='time'>Just now</div>" + msg + 
+							   "</div>");
+      
+	  var objDiv = document.getElementById("messagesContainer");
+      objDiv.scrollTop = objDiv.scrollHeight;
+});   
+
+function isValid(thatemail) {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(thatemail);
+	} 	
+	
+messageTextArea.keypress(function(e){
+  if (e.which !== 13) {        
+        socket.emit("typing", true);
+    } else {		
+		socket.emit('typing', false);
+    }
+});
+
+messageTextArea.focusout(function() {
+    socket.emit('typing', false);
+});
+
+socket.on("isTyping", function(data) {  
+  if (data.isTyping) {
+      $("#typingArea").text(data.userTyping + " печатает...");
+  } else {
+	  $("#typingArea").text('');
+  }
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -9,15 +10,36 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  
+  socket.on('login', function(data) {
+	console.log('a user logon');
+	socket.myName = data.name;
+	users.push(socket.myName);
+	io.emit('logon message', socket.myName+" logon!");
   });
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  
+  socket.on('logon message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('logon message', msg);
+  });
+  
+  socket.on('new message', function(data){
+    console.log('message: ' + data.message);
+    io.emit('new message', data);
+  });
+  
+  socket.on('disconnect', function(data){
+    console.log(socket.myName + ' disconnected');
+	socket.broadcast.emit('disconnect message', socket.myName + ' disconnected');
+  });
+  
+  socket.on("typing", function(data) {  
+		socket.broadcast.emit("isTyping", {isTyping: data, userTyping: socket.myName});
   });
 });
 
+
+		
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on', http.address().port);
 });
